7610979dcd98d9ec8d41e6ba38d806bb5e0cb01a
kuni <kna-gold@outlook.jp>
2026-04-20 10:17:56 +0900

    skillBase、TypeとAttibute調整

--- Project/BaseProject/Src/Object/Skill/SkillBase.h
+++ Project/BaseProject/Src/Object/Skill/SkillBase.h
@@ -1,52 +1,62 @@
 #pragma once
-#include <functional>
-#include <DxLib.h>
-#include<string>
+#include <string>
+#include <map>
 class Player;
 
 class SkillBase
 {
 public:
-
-
 	// 種別
 	enum class TYPE
 	{
-		PHYSICS,
-		FIRE,
-		WATER,
-		WIND,
-		SHINE,
-		DARK,
-		BUFF,
-		DEBUFF
+		NONE,		// 無
+		PHYSICS,	// 物理
+		MAGIC,		// 魔法
 	};
+
+	// 属性
+	enum class ATTRIBUTE
+	{
+		NONE,	// 無
+		FIRE,	// 火
+		WATER,	// 水
+		WIND,	// 風
+		SHINE,	// 光
+		DARK,	// 闇
+		BUFF,	// バフ
+		DEBUFF,	// デバフ
+	};
+
 	//スキルデータ
 	struct SkillData
 	{
-		int id;					// ID
 		std::string skillName;	// スキル名
-		SkillBase::TYPE type;	// スキルタイプ
-		float physicsAtkMag;	//物理攻撃倍率
-		float physicsDefMag;	//物理防御倍率
-		float magicAtkMag;		//魔法攻撃倍率
-		float magicDefMag;		//魔法防御倍率
-		float recoveryMag;		//回復倍率
-		float buffMag;			// バフ倍率
+		TYPE type;				// スキルタイプ
+		ATTRIBUTE attribute;	// スキル属性
+		float physAtkMag;		// 物理攻撃倍率/物理攻撃バフ倍率/物理攻撃デバフ倍率
+		float physDefMag;		// 物理防御倍率/物理防御バフ倍率/物理防御デバフ倍率
+		float magicAtkMag;		// 魔法攻撃倍率/魔法攻撃バフ倍率/魔法攻撃デバフ倍率
+		float magicDefMag;		// 魔法防御倍率/魔法防御バフ倍率/魔法防御デバフ倍率
+		float recoveryMag;		// 回復倍率
 	};
 
 	// コンストラクタ
-	SkillBase();
+	SkillBase(void);
 	//デストラクタ
-	~SkillBase();
+	~SkillBase(void);
+	
+	void Init(void);		// 初期化
+	void Update(void);		// 更新
+	void Draw(void);		// 描画
+	void Release(void);		// 解放
 
 protected:
+	void LoadSkillData(void);	// スキルデータファイルの読み込み
 
 
 
 
-
-	
+	std::map<int, SkillData> skillDataMap_;	// スキルデータ
 
 };
 
