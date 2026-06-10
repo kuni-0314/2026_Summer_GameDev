cbe43ae71253ed5ebafb35eda45b9e441903ad97
kuni <kna-gold@outlook.jp>
2026-06-10 10:43:38 +0900

    エラーまみれプッシュ

--- Project/BaseProject/Src/Object/Item/ItemSkill/SkillItem.cpp
+++ Project/BaseProject/Src/Object/Item/ItemSkill/SkillItem.cpp
@@ -1,153 +1,153 @@
-//#include "../../Charactor/Enemy/EnemyBase.h"
-//#include "../../../Manager/ResourceManager.h"
-//#include "../../../Manager/SceneManager.h"
-//#include "../../Collider/Line/ColliderLine.h"
-//#include "../../Collider/Capsule/ColliderCapsule.h"
-//#include "../../../Utility/AsoUtility.h"
-//#include "../../Charactor/Player/Player.h"
-//#include "SkillItem.h"
-//
-//
-//SkillItem::SkillItem(Player* player)
-//    :player_(player)
-//{
-//}
-//
-//SkillItem::~SkillItem(void)
-//{
-//}
-//
-//void SkillItem::Update(void)
-//{
-//    ItemBase::Update();
-//    //プレイヤーとの判定
-//    //InSearchModel()
-//
-//
-//
-//}
-//
-//void SkillItem::Draw(void)
-//{
-//
-//
-//    ItemBase::Draw();
-//    //DrawSphere3D(transform_.pos,COL_SPHERE_RADIUS, 10, 0x0000ff, 0x0000ff, false);
-//
-//
-//}
-//
-//void SkillItem::InitLoad(void)
-//{
-//    //基底クラスのリソースロード
-//    transform_.SetModel(resMng_.LoadModelDuplicate(ResourceManager::SRC::ITEM_SKILL));
-//
-//}
-//
-//void SkillItem::InitTransform(void)
-//{
-//    //大きさ、座標等の初期化
-//    transform_.scl = { SCALE ,SCALE ,SCALE };
-//    transform_.quaRot = Quaternion::Identity();
-//    transform_.quaRotLocal = Quaternion::Euler(ROT);
-//    jumpPow_ = AsoUtility::VECTOR_ZERO;
-//    transform_.Update();
-//}
-//
-//void SkillItem::InitCollider(void)
-//{
-//    // 主に地面との衝突で仕様する線分コライダ
-//    ColliderLine* colLine = new ColliderLine(
-//        ColliderBase::TAG::ITEM, &transform_,
-//        COL_LINE_START_LOCAL_POS, COL_LINE_END_LOCAL_POS);
-//    ownColliders_.emplace(static_cast<int>(COLLIDER_TYPE::LINE), colLine);
-//
-//    // 主に壁や木などの衝突で仕様するカプセルコライダ
-//    ColliderCapsule* colCapsule = new ColliderCapsule(
-//        ColliderBase::TAG::ITEM, &transform_,
-//        COL_CAPSULE_TOP_LOCAL_POS, COL_CAPSULE_DOWN_LOCAL_POS,
-//        COL_CAPSULE_RADIUS);
-//    ownColliders_.emplace(static_cast<int>(COLLIDER_TYPE::CAPSULE), colCapsule);
-//
-//    // モデルのコライダ
-//    ColliderModel* colModel =
-//        new ColliderModel(ColliderBase::TAG::ITEM, &transform_);
-//    //判定の登録
-//    ownColliders_.emplace(
-//        static_cast<int>(COLLIDER_TYPE::ITEM), colModel);
-//}
-//
-//void SkillItem::InitAnimation(void)
-//{
-//}
-//
-//void SkillItem::InitPost(void)
-//{
-//}
-//
-//void SkillItem::UpdateProcess(void)
-//{
-//
-//	//transform_.rot.y +=  5 * DX_PI_F / 180.0f;
-//
-//    //プレイヤー座標取得
-//    VECTOR playerPos = player_->GetPos();
-//
-//    // プレイヤー方向
-//    VECTOR toPlayer = VSub(playerPos, transform_.pos);
-//
-//    // 距離
-//    float distance = VSize(toPlayer);
-//
-//    // 正規化方向
-//    VECTOR dir = VNorm(toPlayer);
-//
-//    const float SEARCH_RADIUS = 300.0f;
-//    const float MOVE_SPEED = 8.0f;
-//
-//    // 取得前
-//    if (!isGet_)
-//    {
-//        // プレイヤーへ吸い寄せ
-//        if (distance < SEARCH_RADIUS)
-//        {
-//            transform_.pos = VAdd(
-//                transform_.pos,
-//                VScale(dir, MOVE_SPEED));
-//        }
-//
-//
-//        float playerRad = player_->GetCollRadius();
-//
-//        // 接触
-//        if (AsoUtility::IsHitSpheres(playerPos, playerRad, transform_.pos, COL_SPHERE_RADIUS))
-//        {
-//            isGet_ = true;
-//        }
-//    }
-//    else
-//    {
-//        // プレイヤーへめり込むように移動
-//        transform_.pos = VAdd(
-//            transform_.pos,
-//            VScale(dir, MOVE_SPEED * 1.5f));
-//
-//        // 徐々に縮小
-//        transform_.scl.x *= SCL_REDUCTION_SPEED;
-//        transform_.scl.y *= SCL_REDUCTION_SPEED;
-//        transform_.scl.z *= SCL_REDUCTION_SPEED;
-//
-//        // 小さくなったら消す
-//        if (transform_.scl.x < SCL_REDUCTION_MIN)
-//        {
-//            isAlive_ = false;
-//        }
-//    }
-//
-//
-//    transform_.Update();
-//}
-//
-//void SkillItem::UpdateProcessPost(void)
-//{
-//}
+#include "../../Charactor/Enemy/EnemyBase.h"
+#include "../../../Manager/ResourceManager.h"
+#include "../../../Manager/SceneManager.h"
+#include "../../Collider/Line/ColliderLine.h"
+#include "../../Collider/Capsule/ColliderCapsule.h"
+#include "../../../Utility/AsoUtility.h"
+#include "../../Charactor/Player/Player.h"
+#include "SkillItem.h"
+
+
+SkillItem::SkillItem(Player* player)
+    :player_(player)
+{
+}
+
+SkillItem::~SkillItem(void)
+{
+}
+
+void SkillItem::Update(void)
+{
+    ItemBase::Update();
+    //プレイヤーとの判定
+    //InSearchModel()
+
+
+
+}
+
+void SkillItem::Draw(void)
+{
+
+
+    ItemBase::Draw();
+    //DrawSphere3D(transform_.pos,COL_SPHERE_RADIUS, 10, 0x0000ff, 0x0000ff, false);
+
+
+}
+
+void SkillItem::InitLoad(void)
+{
+    //基底クラスのリソースロード
+    transform_.SetModel(resMng_.LoadModelDuplicate(ResourceManager::SRC::ITEM_SKILL));
+
+}
+
+void SkillItem::InitTransform(void)
+{
+    //大きさ、座標等の初期化
+    transform_.scl = { SCALE ,SCALE ,SCALE };
+    transform_.quaRot = Quaternion::Identity();
+    transform_.quaRotLocal = Quaternion::Euler(ROT);
+    jumpPow_ = AsoUtility::VECTOR_ZERO;
+    transform_.Update();
+}
+
+void SkillItem::InitCollider(void)
+{
+    // 主に地面との衝突で仕様する線分コライダ
+    ColliderLine* colLine = new ColliderLine(
+        ColliderBase::TAG::ITEM, &transform_,
+        COL_LINE_START_LOCAL_POS, COL_LINE_END_LOCAL_POS);
+    ownColliders_.emplace(static_cast<int>(COLLIDER_TYPE::LINE), colLine);
+
+    // 主に壁や木などの衝突で仕様するカプセルコライダ
+    ColliderCapsule* colCapsule = new ColliderCapsule(
+        ColliderBase::TAG::ITEM, &transform_,
+        COL_CAPSULE_TOP_LOCAL_POS, COL_CAPSULE_DOWN_LOCAL_POS,
+        COL_CAPSULE_RADIUS);
+    ownColliders_.emplace(static_cast<int>(COLLIDER_TYPE::CAPSULE), colCapsule);
+
+    // モデルのコライダ
+    ColliderModel* colModel =
+        new ColliderModel(ColliderBase::TAG::ITEM, &transform_);
+    //判定の登録
+    ownColliders_.emplace(
+        static_cast<int>(COLLIDER_TYPE::ITEM), colModel);
+}
+
+void SkillItem::InitAnimation(void)
+{
+}
+
+void SkillItem::InitPost(void)
+{
+}
+
+void SkillItem::UpdateProcess(void)
+{
+
+	//transform_.rot.y +=  5 * DX_PI_F / 180.0f;
+
+    //プレイヤー座標取得
+    VECTOR playerPos = player_->GetPos();
+
+    // プレイヤー方向
+    VECTOR toPlayer = VSub(playerPos, transform_.pos);
+
+    // 距離
+    float distance = VSize(toPlayer);
+
+    // 正規化方向
+    VECTOR dir = VNorm(toPlayer);
+
+    const float SEARCH_RADIUS = 300.0f;
+    const float MOVE_SPEED = 8.0f;
+
+    // 取得前
+    if (!isGet_)
+    {
+        // プレイヤーへ吸い寄せ
+        if (distance < SEARCH_RADIUS)
+        {
+            transform_.pos = VAdd(
+                transform_.pos,
+                VScale(dir, MOVE_SPEED));
+        }
+
+
+        float playerRad = player_->GetCollRadius();
+
+        // 接触
+        if (AsoUtility::IsHitSpheres(playerPos, playerRad, transform_.pos, COL_SPHERE_RADIUS))
+        {
+            isGet_ = true;
+        }
+    }
+    else
+    {
+        // プレイヤーへめり込むように移動
+        transform_.pos = VAdd(
+            transform_.pos,
+            VScale(dir, MOVE_SPEED * 1.5f));
+
+        // 徐々に縮小
+        transform_.scl.x *= SCL_REDUCTION_SPEED;
+        transform_.scl.y *= SCL_REDUCTION_SPEED;
+        transform_.scl.z *= SCL_REDUCTION_SPEED;
+
+        // 小さくなったら消す
+        if (transform_.scl.x < SCL_REDUCTION_MIN)
+        {
+            isAlive_ = false;
+        }
+    }
+
+
+    transform_.Update();
+}
+
+void SkillItem::UpdateProcessPost(void)
+{
+}
